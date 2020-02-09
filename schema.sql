c6b70db6820d569ac922b6d73c353521d0de40ea
the137 <eschwelgin@gmail.com>
2020-02-09 13:00:57 -0500

    initial commit

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE database employee_db;
+USE employee_db; 
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id INT NOT NULL, 
+    manager_id INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary decimal NOT NULL, 
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name varchar(30),
+    PRIMARY KEY(id)
+);
+
