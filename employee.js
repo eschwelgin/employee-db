66068320b60ebae65fa11c84b7d873a11de7d775
[eschwelgin] <[eschwelgin@gmail.com]>
2020-02-10 19:25:45 -0500

    built out inquirer

--- employee.js
+++ employee.js
@@ -1,3 +1,154 @@
 const mysql = require('mysql')
 const inquirer = require('inquirer')
 
+function choiceFn(choice) {
+    switch (choice) {
+        case "View All Employees": 
+            viewAll()
+            break;
+        case "View Employees by Department":
+            viewByDept()
+            break;
+        case "View Employees by Manager": 
+            viewByManager()
+            break;
+        case "Add Employee": 
+            addEmp()
+            break;
+        case "Remove Employee": 
+            removeEmp()
+            break;
+        case "Update Employee Role": 
+            updateRole()
+            break;
+        case "Update Employee Manager": 
+            updateManager() 
+            break;
+        case "Exit":
+            console.log("exiting")
+            break; 
+        
+        
+        default: 
+            console.log("defaulting, probably an error")
+    }
+}
+
+function viewAll() {
+    console.log("Displaying All Employees")
+
+    mainMenu()
+}
+
+function viewByDept() {
+    console.log("Displaying Employees by Department")
+
+    mainMenu()
+}
+
+function viewByManager() {
+    console.log("view emp by manager")
+
+    mainMenu()
+}
+
+function addEmp() {
+    const questions = [
+        {
+            type: "input",
+            message: "What is the employee's first name?",
+            name: "firstName"
+        }, 
+        {
+            type: "input",
+            message: "What is the employee's last name?",
+            name: "lastName"
+        },
+        {
+            type: "list",
+            message: "What is the employee's role?",
+            name: "empRole",
+            choices: ["map fn"]
+        }, 
+        {
+            type: "list", 
+            message: "Who is the employee's manager?",
+            name: "empManager",
+            choices: ["map fn"]
+        }
+    ];
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log(answers)
+    })
+}
+
+function removeEmp() {
+    console.log("rem emp")
+
+    mainMenu()
+}
+
+function updateRole() {
+    console.log("updating role")
+
+    mainMenu()
+}
+
+function updateManager() {
+    console.log("updating manager")
+
+    mainMenu()
+}
+
+function mainMenu() {
+    const questions = [
+        {
+            type: "list",
+            message: "What Would You Like to Do?",
+            name: "mainList",
+            choices: [
+                "View All Employees",
+                "View Employees by Department",
+                "View Employees by Manager",
+                "Add Employee",
+                "Remove Employee", 
+                "Update Employee Role",
+                "Update Employee Manager", 
+                "Exit"
+            ]
+        }
+    ];
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        // console.log(answers.mainList)
+        choiceFn(answers.mainList)
+    })
+}
+
+function start() {
+    console.log(`
+             ---------------------------------------,
+            / -----------------------------------/ /|
+           / /|? ? SCHRODINGER'S EMPLOYEES ? ? ?/ /*|
+          / /*|?     ARE THERE EMPLOYEES      ?/ /**|
+         / /**|?          IN HERE            ?/ /***|
+        / /***|_?_?_?_?_?_?_?_?_?_?_?_?_?_?_?/ /****|
+       / /***/                              / /*****/
+      / /***/                              / /*****/
+     / /***/                              / /*****/ 
+    / /***/                              / /*****/
+   / /***/                              / /*****/ 
+  / /*_*/_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ / /*****/
+ /_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ /*****/
+|                                       |****/
+|                                       |***/
+|                                       |**/
+|                        lets hope so...|*/
+|---------------------------------------|/
+`)
+mainMenu()
+}
+start()
--- schema.sql
+++ schema.sql
@@ -1,26 +1,58 @@
-CREATE database employee_db;
+DROP DATABASE employee_db;
+CREATE DATABASE employee_db;
 USE employee_db; 
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    dept_name varchar(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE employee_role (
+    id INT AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary decimal NOT NULL, 
+    department_id INT, 
+    CONSTRAINT fk_dept
+    FOREIGN KEY (department_id)
+		REFERENCES department(id),
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL,
-    role_id INT NOT NULL, 
-    manager_id INT,
+    employee_role INT,
+	CONSTRAINT fk_role 
+	FOREIGN KEY (employee_role)
+		REFERENCES employee_role(id),
+	manager_id INT, 
+	CONSTRAINT fk_manager
+	FOREIGN KEY (manager_id)
+		REFERENCES employee(id), 
     PRIMARY KEY(id)
 );
 
-CREATE TABLE role(
-    id INT AUTO_INCREMENT,
-    title varchar(30) NOT NULL,
-    salary decimal NOT NULL, 
-    PRIMARY KEY(id)
-);
+INSERT INTO department(dept_name)
+    VALUES("Development"),
+            ("Sales"),
+            ("Customer Support");
+INSERT INTO employee_role(title, salary, department_id)
+    VALUES ("Lead Developer", 100000, 1),
+            ("Developer", 75000, 1), 
+            ("Sales Manager", 80000, 2),
+            ("Customer Support Manager", 60000, 3),
+            ("Salesman", 40000, 2), 
+            ("Customer Support Specialist", 32000, 3);
+INSERT INTO employee(first_name, last_name, employee_role, manager_id)
+    VALUES("Eric", "Schwelgin", 1, 1),
+            ("Katie", "Baker", 4, 1),
+            ("John", "Lopo", 3, 2),
+            ("Bobby", "BootCamp", 2, 1), 
+            ("Jacob", "BootCamp", 2, 1), 
+            ("Eric", "Susa", 2, 1), 
+            ("Mike", "Wapo", 2, 6),
+            ("Shane", "Dirt", 2, 6),
+            ("Chris", "Dirt", 3, 2);
 
-CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name varchar(30),
-    PRIMARY KEY(id)
-);
-
-INSERT INTO 
